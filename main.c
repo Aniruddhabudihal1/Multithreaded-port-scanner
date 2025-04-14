90b81580d59f981ce5760be2d726f6c720d2f876
Aniruddha Budihal <aniruddhabudihal@gmail.com>
2025-04-14 16:16:32 +0530

    Final Final project

--- main.c
+++ main.c
@@ -5,63 +5,14 @@ int main() {
     printf("Choose an option:\n");
     printf("1. Domain Port Scan\n");
     printf("2. Default Local Subnet Scan\n");
-    printf("3. Custom Port Scan\n");
     printf("Enter your choice: ");
     scanf("%d", &choice);
     
     if (choice == 1) {
         domain_parser();
-        
-        // After parsing domain, get IP to scan
-        char address[WEB_ADDRESS];
-        printf("\nEnter the IP address from above to scan: ");
-        scanf("%s", address);
-        
-        int start_port = 1;
-        int end_port = 1024; // Default: scan well-known ports
-        int num_threads = DEFAULT_THREADS;
-        
-        printf("Enter start port (1-65535): ");
-        scanf("%d", &start_port);
-        
-        printf("Enter end port (1-65535): ");
-        scanf("%d", &end_port);
-        
-        printf("Enter number of threads (1-100): ");
-        scanf("%d", &num_threads);
-        
-        if (start_port < 1) start_port = 1;
-        if (end_port > MAX_PORTS) end_port = MAX_PORTS;
-        if (num_threads < 1) num_threads = 1;
-        if (num_threads > MAX_THREADS) num_threads = MAX_THREADS;
-        
-        scan_target(address, start_port, end_port, num_threads);
     } 
     else if (choice == 2) {
         defaultScan();
-    }
-    else if (choice == 3) {
-        char address[WEB_ADDRESS];
-        int start_port, end_port, num_threads;
-        
-        printf("Enter target IP address: ");
-        scanf("%s", address);
-        
-        printf("Enter start port (1-65535): ");
-        scanf("%d", &start_port);
-        
-        printf("Enter end port (1-65535): ");
-        scanf("%d", &end_port);
-        
-        printf("Enter number of threads (1-100): ");
-        scanf("%d", &num_threads);
-        
-        if (start_port < 1) start_port = 1;
-        if (end_port > MAX_PORTS) end_port = MAX_PORTS;
-        if (num_threads < 1) num_threads = 1;
-        if (num_threads > MAX_THREADS) num_threads = MAX_THREADS;
-        
-        scan_target(address, start_port, end_port, num_threads);
     }
     else {
         printf("Invalid option selected.\n");
--- scanner.c
+++ scanner.c
@@ -59,7 +59,7 @@ void* port_scan_thread(void* args) {
                 getsockopt(sock, SOL_SOCKET, SO_ERROR, &so_error, &len);
                 
                 if (so_error == 0) {
-                    printf("Port %d is open\n", port);
+                    printf("Port %d is open on %s\n", port, address);
                 }
             }
         }
@@ -83,6 +83,7 @@ void scan_target(const char* address, int start_port, int end_port, int num_thre
     }
     
     int ports_per_thread = (end_port - start_port + 1) / num_threads;
+    if (ports_per_thread < 1) ports_per_thread = 1;
     
     printf("Starting multithreaded port scan on %s (ports %d-%d) with %d threads\n", 
            address, start_port, end_port, num_threads);
@@ -90,9 +91,17 @@ void scan_target(const char* address, int start_port, int end_port, int num_thre
     // Create and start threads
     for (int i = 0; i < num_threads; i++) {
         thread_args[i].start_port = start_port + (i * ports_per_thread);
-        thread_args[i].end_port = (i == num_threads - 1) ? 
-                                  end_port : 
-                                  start_port + ((i + 1) * ports_per_thread - 1);
+        
+        // Make sure we don't exceed the end_port
+        if (i == num_threads - 1) {
+            thread_args[i].end_port = end_port;
+        } else {
+            thread_args[i].end_port = start_port + ((i + 1) * ports_per_thread - 1);
+            if (thread_args[i].end_port > end_port) {
+                thread_args[i].end_port = end_port;
+            }
+        }
+        
         thread_args[i].thread_id = i + 1;
         strncpy(thread_args[i].address, address, WEB_ADDRESS - 1);
         thread_args[i].address[WEB_ADDRESS - 1] = '\0';
@@ -111,7 +120,87 @@ void scan_target(const char* address, int start_port, int end_port, int num_thre
     printf("Port scan completed on %s\n", address);
 }
 
-// Default scan for subnet
+// Function to check if a host is up by scanning it (used by defaultScan)
+int host_is_up(const char* address, int quick_check) {
+    struct timeval timeout;
+    timeout.tv_sec = 0;  // Use very short timeout for quick check
+    timeout.tv_usec = quick_check ? 100000 : 300000;  // 100ms or 300ms
+    
+    // Try common ports: 80 (HTTP), 443 (HTTPS), 22 (SSH), 21 (FTP)
+    int common_ports[] = {80, 443, 22, 21};
+    int num_ports = quick_check ? 1 : sizeof(common_ports) / sizeof(common_ports[0]);
+    
+    for (int i = 0; i < num_ports; i++) {
+        int sock = socketCreate();
+        setsockopt(sock, SOL_SOCKET, SO_SNDTIMEO, &timeout, sizeof(timeout));
+        setsockopt(sock, SOL_SOCKET, SO_RCVTIMEO, &timeout, sizeof(timeout));
+        
+        int flags = fcntl(sock, F_GETFL, 0);
+        fcntl(sock, F_SETFL, flags | O_NONBLOCK);
+        
+        struct sockaddr_in remote = {0};
+        remote.sin_addr.s_addr = inet_addr(address);
+        remote.sin_family = AF_INET;
+        remote.sin_port = htons(common_ports[i]);
+        
+        if (connect(sock, (struct sockaddr*)&remote, sizeof(struct sockaddr_in)) == 0 || 
+            errno == EINPROGRESS) {
+            
+            fd_set fdset;
+            FD_ZERO(&fdset);
+            FD_SET(sock, &fdset);
+            
+            if (select(sock + 1, NULL, &fdset, NULL, &timeout) == 1) {
+                int so_error;
+                socklen_t len = sizeof(so_error);
+                getsockopt(sock, SOL_SOCKET, SO_ERROR, &so_error, &len);
+                
+                if (so_error == 0) {
+                    close(sock);
+                    return 1;  // Host is up
+                }
+            }
+        }
+        close(sock);
+    }
+    return 0;  // Host is down
+}
+
+// Structure to pass data to sweep threads
+typedef struct {
+    int thread_id;
+    int start_host;
+    int end_host;
+    int blockOne;
+    int blockTwo;
+    int blockThree;
+    char (*active_hosts)[16];
+    int *count;
+    pthread_mutex_t *mutex;
+} SweepThreadArgs;
+
+// Thread function for ping sweep
+void* ping_sweep_thread(void* arg) {
+    SweepThreadArgs* data = (SweepThreadArgs*)arg;
+    
+    for (int host = data->start_host; host <= data->end_host; host++) {
+        char ip[16];
+        sprintf(ip, "%d.%d.%d.%d", data->blockOne, data->blockTwo, data->blockThree, host);
+        
+        if (host_is_up(ip, 1)) {  // Quick check
+            pthread_mutex_lock(data->mutex);
+            if (*(data->count) < 255) {
+                strcpy(data->active_hosts[*(data->count)], ip);
+                (*(data->count))++;
+                printf("Host found: %s\n", ip);
+            }
+            pthread_mutex_unlock(data->mutex);
+        }
+    }
+    return NULL;
+}
+
+// Default scan for subnet with common ports
 void defaultScan() {
     printf("Scanning the local subnet for devices...\n");
     printf("This will take some time\n");
@@ -120,10 +209,13 @@ void defaultScan() {
     int blockOne = 192;
     int blockTwo = 168;
     int blockThree = 1;
-    int blockFour = 1;
     
     char address[16];
-    int num_threads = 5; // Use fewer threads for subnet scanning
+    int num_threads = 8;  // Number of threads for host discovery
+    
+    // Common ports to scan
+    int common_ports[] = {21, 22, 23, 25, 53, 80, 110, 111, 135, 139, 143, 443, 445, 993, 995, 1723, 3306, 3389, 5900, 8080};
+    int num_common_ports = sizeof(common_ports) / sizeof(common_ports[0]);
     
     // Get local subnet (optional enhancement)
     char hostname[256];
@@ -136,43 +228,71 @@ void defaultScan() {
         char* ip = inet_ntoa(addr);
         
         // Parse IP to get subnet
-        sscanf(ip, "%d.%d.%d.%d", &blockOne, &blockTwo, &blockThree, &blockFour);
-        blockFour = 1; // Start from 1
+        sscanf(ip, "%d.%d.%d.%d", &blockOne, &blockTwo, &blockThree, &blockThree);
+        // We reset blockThree later if needed
     }
     
-    printf("Starting subnet scan from %d.%d.%d.%d\n", blockOne, blockTwo, blockThree, blockFour);
+    printf("Starting subnet scan from %d.%d.%d.1\n", blockOne, blockTwo, blockThree);
     
-    pthread_t threads[255];
-    ThreadArgs thread_args[255];
-    int thread_count = 0;
+    // First, do a quick ping sweep to find active hosts
+    printf("Performing quick ping sweep to find active hosts...\n");
     
-    // Scan a fixed range (e.g., 192.168.1.1 to 192.168.1.254)
-    for (int host = 1; host <= 254; host++) {
-        sprintf(address, "%d.%d.%d.%d", blockOne, blockTwo, blockThree, host);
+    char active_hosts[255][16];  // Store active IP addresses
+    int active_host_count = 0;
+    
+    // Ping sweep with multiple threads
+    pthread_t sweep_threads[MAX_THREADS];
+    SweepThreadArgs sweep_args[MAX_THREADS];
+    pthread_mutex_t mutex = PTHREAD_MUTEX_INITIALIZER;
+    
+    int hosts_per_thread = 254 / num_threads;
+    if (hosts_per_thread < 1) hosts_per_thread = 1;
+    
+    // Create threads for ping sweep
+    for (int i = 0; i < num_threads; i++) {
+        sweep_args[i].thread_id = i;
+        sweep_args[i].start_host = 1 + (i * hosts_per_thread);
+        sweep_args[i].end_host = (i == num_threads - 1) ? 254 : 
+                               sweep_args[i].start_host + hosts_per_thread - 1;
+        sweep_args[i].blockOne = blockOne;
+        sweep_args[i].blockTwo = blockTwo;
+        sweep_args[i].blockThree = blockThree;
+        sweep_args[i].active_hosts = active_hosts;
+        sweep_args[i].count = &active_host_count;
+        sweep_args[i].mutex = &mutex;
         
-        // Set up arguments for this thread
-        thread_args[thread_count].start_port = 80; // Just scan port 80 for hosts by default
-        thread_args[thread_count].end_port = 80;
-        thread_args[thread_count].thread_id = thread_count + 1;
-        strncpy(thread_args[thread_count].address, address, WEB_ADDRESS - 1);
-        thread_args[thread_count].address[WEB_ADDRESS - 1] = '\0';
-        
-        if (pthread_create(&threads[thread_count], NULL, port_scan_thread, &thread_args[thread_count]) != 0) {
-            perror("Failed to create thread");
+        if (pthread_create(&sweep_threads[i], NULL, ping_sweep_thread, &sweep_args[i]) != 0) {
+            perror("Failed to create sweep thread");
             exit(EXIT_FAILURE);
         }
+    }
+    
+    // Wait for all ping sweep threads to complete
+    for (int i = 0; i < num_threads; i++) {
+        pthread_join(sweep_threads[i], NULL);
+    }
+    
+    printf("Found %d active hosts\n", active_host_count);
+    
+    // Now scan common ports on active hosts
+    printf("Scanning common ports on active hosts...\n");
+    
+    for (int i = 0; i < active_host_count; i++) {
+        printf("Scanning ports on %s\n", active_hosts[i]);
         
-        thread_count++;
-        
-        // Limit active threads
-        if (thread_count % 20 == 0 || host == 254) {
-            // Wait for current batch to complete
-            for (int i = 0; i < thread_count; i++) {
-                pthread_join(threads[i], NULL);
-            }
-            thread_count = 0;
+        // Create a single thread to scan all common ports
+        for (int j = 0; j < num_common_ports; j++) {
+            ThreadArgs port_args;
+            port_args.start_port = common_ports[j];
+            port_args.end_port = common_ports[j];
+            port_args.thread_id = 1;
+            strncpy(port_args.address, active_hosts[i], WEB_ADDRESS - 1);
+            port_args.address[WEB_ADDRESS - 1] = '\0';
+            
+            port_scan_thread(&port_args);
         }
     }
     
+    pthread_mutex_destroy(&mutex);
     printf("Subnet scan completed\n");
 }
